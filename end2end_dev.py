1b432a7fc7e5f69079c63f050b313dae9ebf202a
MA Song <song.ma.18@ucl.ac.uk>
2022-05-15 20:16:34 +0100

    comparing max and sum in tensorboard

--- end2end_dev.py
+++ end2end_dev.py
@@ -38,11 +38,12 @@ if __name__ == '__main__':
         'embedding_dim': 128,
         'hidden_dim': 128,
         'n_components': 3,
+        'cost_d_op': 'max'
     }
 
     options = {
         'model_type': 'moe_mlp',
-        'data_type': 'blob',
+        'data_type': 'random',
         'log_dir': 'logs',
         'checkpoint_interval': 200,
         'gradient_check_flag': True,
@@ -51,6 +52,8 @@ if __name__ == '__main__':
     assert options['model_type'] in {'moe_mlp', 'mlp', 'attention'}, "model_type: {}, does not exist"\
         .format(options['model_type'])
     assert options['data_type'] in {'random', 'blob'}, "data_type: {}, does not exist".format(options['data_type'])
+    assert hyper_params['cost_d_op'] in {'sum', 'max'}, "cost_d_op: {}, does not exist"\
+        .format(hyper_params['cost_d_op'])
 
     eps = np.finfo(np.float32).eps.item()
     cur_time = datetime.now() + timedelta(hours=0)
@@ -120,7 +123,8 @@ if __name__ == '__main__':
         _, _, Rcc, Rco = dense_mincut_pool(X, adj_norm, cluster_policy_logits)
 
         # initialise the tensor to store the total distance
-        cost_d = torch.tensor(data=np.zeros(X.shape[0]))
+        cost_d_max = torch.tensor(data=np.zeros(X.shape[0]))
+        cost_d_sum = torch.tensor(data=np.zeros(X.shape[0]))
 
         degeneration_count = 0
         for m in range(X.shape[0]):
@@ -155,14 +159,24 @@ if __name__ == '__main__':
                     pi.append(pi_i)
                     R_d.append(dist_i)
 
-            cost_d[m] = torch.tensor(max(R_d), dtype=torch.float32)
+            cost_d_max[m] = torch.tensor(max(R_d), dtype=torch.float32)
+            cost_d_sum[m] = torch.tensor(sum(R_d), dtype=torch.float32)
 
         degeneration_ratio = degeneration_count/(X.shape[0] * hyper_params['num_clusters'])
-        print("----------cost_d:::", cost_d.mean().item(), "----------degeneration_ratio:::", degeneration_ratio)
+        print("----------cost_d:::", cost_d_max.mean().item(), "----------degeneration_ratio:::", degeneration_ratio)
 
         writer.add_scalar('degeneration_ratio', degeneration_ratio, batch_id)
 
-        cost_d_log = cost_d.mean().item()
+        cost_d_max_log = cost_d_max.mean().item()
+        cost_d_sum_log = cost_d_sum.mean().item()
+
+        if hyper_params['cost_d_op'] == 'max':
+            cost_d = cost_d_max
+        elif hyper_params['cost_d_op'] == 'sum':
+            cost_d = cost_d_sum
+        else:
+            raise ValueError("Wrong 'cost_d_op' value")
+
         cost_d = (cost_d - cost_d.mean()) / (cost_d.std() + eps)
         cost = (1 - lamb) * cost_d + lamb * (Rcc + Rco)
 
@@ -185,7 +199,8 @@ if __name__ == '__main__':
 
         cost_log = cost.mean().item()
         writer.add_scalar('lamb', lamb, batch_id)
-        writer.add_scalar('cost_d', cost_d_log, batch_id)
+        writer.add_scalar('cost_d_max', cost_d_max_log, batch_id)
+        writer.add_scalar('cost_d_sum', cost_d_sum_log, batch_id)
         writer.add_scalar('training_cost', cost_log, batch_id)
         writer.add_scalar('training_rl_loss', reinforce_loss.item(), batch_id)
         writer.add_scalar('grad_norm', grad_norms[0][0].item(), batch_id)
