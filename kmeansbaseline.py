ff6503e42d62818cf8ef5add03b353e4699ae45e
MA Song <song.ma.18@ucl.ac.uk>
2022-06-13 19:31:06 +0100

    checkpoint

--- kmeansbaseline.py
+++ kmeansbaseline.py
@@ -5,10 +5,20 @@ import pickle
 import numpy as np
 from sklearn.cluster import KMeans
 
+from tsp_solver import pointer_tsp_solve
+
 
 class Benchmarking:
     def __int__(self, dir_dataset, dir_args):
         self._import_dataset_args(dir_dataset, dir_args)
+
+        self.data_batches = self.dataset.data.numpy()
+        self.batch_size = self.data_batches.shape[0]
+
+        self._k_means_clustering()
+
+        for batch in range(self.batch_size):
+            self._cluster_tsp_solving(batch)
 
     def _import_dataset_args(self, dir_dataset: str, dir_args: str):
         assert os.path.splitext(dir_dataset)[1] == '.pkl', "Should be a pickled file."
@@ -18,16 +28,35 @@ class Benchmarking:
         self.dataset = pickle.load(open(dir_dataset, 'rb'))
         self.args = pickle.load(open(dir_args, 'rb'))
 
-    def k_means_clustering(self):
-        kmeans = KMeans(n_clusters=self.args.clusters_num)
-        data_batches = self.dataset.data.numpy()
-        labels = np.zeros_like(data_batches, dtype=int)
-        for batch in range(data_batches.shape[0]):
-            data = data_batches[batch]
-            labels[batch] = kmeans.fit_predict(data)
+    def _k_means_clustering(self):
+        self.kmeans = KMeans(n_clusters=self.args.clusters_num)
+        self.labels = np.zeros_like(self.data_batches, dtype=int)
+        for batch in range(self.batch_size):
+            data = self.data_batches[batch]
+            self.labels[batch] = self.kmeans.fit_predict(data)
 
-    def tsp_solving(self):
-        pass
+    def _cluster_tsp_solving(self, batch):
+        labels = self.labels[batch]
+        c_d = []
+        c_d_origin = []
+        x_c = []
+        pi = []
+        for cluster in range(self.args.clusters_num):
+            ind_c = np.argwhere(labels == cluster).squeeze()
+
+            if sum(ind_c.shape) == 0:
+                degeneration_flag = True
+                c_d.append(self.args.degeneration_penalty)
+                c_d_origin.append(0)
+                # degeneration_count += 1
+            else:
+                x_i = self.data_batches[batch, ind_c, :]
+                x_c.append(x_i)
+                pi_i, dist_i = pointer_tsp_solve(x_i.numpy())
+
+                pi.append(pi_i)
+                c_d.append(dist_i)
+                c_d_origin.append(dist_i)
 
 
 if __name__ == '__main__':
