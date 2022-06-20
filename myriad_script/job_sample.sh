2b827924e763891815ec5d4d301e2961943fd116
MA Song <song.ma.18@ucl.ac.uk>
2022-06-20 13:41:10 +0100

    update myriad script sample

--- myriad_script/job_sample.sh
+++ myriad_script/job_sample.sh
@@ -1,9 +1,9 @@
-#$ -l mem=64G
+#$ -l mem=128G
 #$ -l h_rt=12:0:0
-#$ -N train_200_3
+#$ -N train_100_3
 #$ -wd /home/ucemaxx/Scratch/GNNClustering
 
 source $HOME/anaconda3/etc/profile.d/conda.sh
 conda activate GNNClustering
 
-python /home/ucemaxx/Scratch/GNNClustering/main.py --train -N 200 -M 64 -k 3 --sample_num 2000000 --penalty_score 35.0
+python /home/ucemaxx/Scratch/GNNClustering/main.py --train -N 100 -M 64 --model_type mlp --lamb 0
