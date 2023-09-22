4f0f0e47c809982c34238a3bb26502db643d165e
MA Song <song.ma.18@ucl.ac.uk>
2023-09-22 12:25:24 +0100

    update myraid script

--- training_scripts/myriad_script/job_sample.sh
+++ training_scripts/myriad_script/job_sample.sh
@@ -1,9 +1,32 @@
-#$ -l mem=128G
-#$ -l h_rt=12:0:0
-#$ -N train_100_3
-#$ -wd /home/ucemaxx/Scratch/GNNClustering
+#!/bin/bash -l
 
-source $HOME/anaconda3/etc/profile.d/conda.sh
-conda activate GNNClustering
+# Batch script to run an OpenMP threaded job under SGE.
 
-python /home/ucemaxx/Scratch/GNNClustering/main.py --train -N 100 -M 64 --model_type mlp --lamb 0
+# Request wallclock time (format hours:minutes:seconds).
+#$ -l h_rt=40:0:0
+
+# Request N gigabyte of RAM for each core/thread 
+# (must be an integer followed by M, G, or T)
+#$ -l mem=192G
+
+# Request 15 gigabyte of TMPDIR space (default is 10 GB - remove if cluster is diskless)
+#$ -l tmpfs=15G
+
+# Set the name of the job.
+#$ -N train_1e_100_5
+
+# Request for cores.
+#$ -pe smp 36
+
+# Request 1 GPU
+#$ -l gpu=1
+
+# Set the working directory to somewhere in your scratch space.
+# Replace "<your_UCL_id>" with your UCL user ID
+#$ -wd /home/<your_UCL_id>/codes/rl_waypoint_mrta
+
+# Load python
+module load python/3.9.10
+
+# 8. Run the application.
+source training_scripts/train.bash
