8a9f22fbfbdfab7c0c23252484774c6bc6ca7711
MA Song <song.ma.18@ucl.ac.uk>
2023-09-23 17:06:55 +0100

    support nohup

--- training_scripts/train_1e_100-4.bash
+++ training_scripts/train_1e_100-4.bash
@@ -1,14 +1,11 @@
 #!/bin/bash
-# Copyright 2023 MA Song at UCL FRL
-
-# nohup bash -c "source training_scripts/train.bash" &
-
 source .venv/bin/activate
 python main.py \
             --train \
             --city_num 100 \
-            --clusters_num 5 \
+            --clusters_num 4 \
             --batch_size 1024 \
             --model_type moe_mlp \
             --hidden_dim 128 \
+            --n_component 1 \
             --checkpoint_interval 10
